b0ecfa034c43f76c6aced3b999326f4e61ca1f73
George Christoforidis <gtg_cerint@hotmail.com>
2023-11-10 17:13:06 +0000

    brute forece with no plugs

---
+++ EnigmaMachineSLN/EnigmaMachinePrj/EnigmaMachine.cs
@@ -0,0 +1,333 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnigmaMachinePrj
+    {
+    class EnigmaMachine
+        {
+        /* Enigma Machine
+           Modelled after Enigma I, from ~1930
+        */
+        private Dictionary<Char, Char> plugBoard;
+
+        // The machine has three rotors and a reflector
+        private Rotor[] rotors;
+        private Rotor reflector;
+
+        private const string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+        // Rotor and reflectors. These configurations are constant and the same on every Enigma machine
+        private const string rotorIconf = "EKMFLGDQVZNTOWYHXUSPAIBRCJ";
+        private const string rotorIIconf = "AJDKSIRUXBLHWTMCQGZNPYFVOE";
+        private const string rotorIIIconf = "BDFHJLCPRTXVZNYEIWGAKMUSQO";
+
+        private const string reflectorAconf = "EJMZALYXVBWFCRQUONTSPIKHGD";
+        private const string reflectorBconf = "YRUHQSLDPXNGOKMIEBFZCWVJAT";
+        private const string reflectorCconf = "FVPJIAOYEDRZXWGCTKUQSBNMHL";
+
+        // Rotor class representing one rotor
+        private class Rotor
+            {
+            // The current char of the alphabet, and position of it. This char is visible outside the machine
+            private int outerPosition;
+            public char outerChar { get; set; }
+
+            // The fixed alphabet of the rotor
+            private string wiring;
+
+            // turnOver is the notch on which letter the rotors turnover point is
+            private char turnOver;
+
+            public string name { get; }
+
+            // Ring is the wiring setting relative to the turnover notch and position
+            // Basically part of the initialization vector
+            public char ring { get; set; }
+
+            public int[] map { get; }
+            public int[] revMap { get; }
+
+            public Rotor(string w, char to, string n)
+                {
+                turnOver = to;
+                outerPosition = 0;
+
+                ring = 'A'; // A default ring setting
+                name = n;
+
+                map = new int[26];
+                revMap = new int[26];
+
+                setWiring(w);
+                }
+
+            public void setWiring(string newW)
+                {
+                wiring = newW;
+                outerChar = wiring.ToCharArray()[outerPosition];
+
+                // Fill the mapping arrays
+                for (int i = 0; i < 26; i++)
+                    {
+                    int match = ((int)wiring.ToCharArray()[i]) - 65;
+                    map[i] = (26 + match - i) % 26;
+                    revMap[match] = (26 + i - match) % 26;
+                    }
+                }
+
+            public void setOuterPosition(int i)
+                {
+                outerPosition = i;
+                outerChar = alphabet.ToCharArray()[outerPosition];
+                }
+
+            public int getOuterPosition()
+                {
+                return outerPosition;
+                }
+
+            public void setOuterChar(char c)
+                {
+                outerChar = c;
+                outerPosition = alphabet.IndexOf(outerChar);
+                }
+
+            public void step()
+                {
+                outerPosition = (outerPosition + 1) % 26;
+                outerChar = alphabet.ToCharArray()[outerPosition];
+                }
+
+            public bool isInTurnOver()
+                {
+                return outerChar == turnOver;
+                }
+            }
+
+        private void rotateRotors(Rotor[] r)
+            {
+            if (r.Length == 3)
+                {
+                if (r[1].isInTurnOver())
+                    {
+                    // If rotor II is on turnOver, all rotors step
+                    r[0].step();
+                    r[1].step();
+                    }
+                else if (r[2].isInTurnOver())
+                    {
+                    // If rotor III is on turnOver, the two rotors to the right step
+                    r[1].step();
+                    }
+
+                // Rotor III always steps
+                r[2].step();
+                }
+            }
+
+        // Apply the rotor scramble to character using all three rotors
+        // Argumentent reverse decides which direction we are scrambling
+        private char rotorMap(char c, bool reverse)
+            {
+            int cPos = (int)c - 65;
+            if (!reverse)
+                {
+                for (int i = rotors.Length - 1; i >= 0; i--)
+                    {
+                    cPos = rotorValue(rotors[i], cPos, reverse);
+                    }
+                }
+            else
+                {
+                for (int i = 0; i < rotors.Length; i++)
+                    {
+                    cPos = rotorValue(rotors[i], cPos, reverse);
+                    }
+                }
+
+            return alphabet.ToCharArray()[cPos];
+            }
+
+        private int rotorValue(Rotor r, int cPos, bool reverse)
+            {
+            int rPos = (int)r.ring - 65;
+            int d;
+            if (!reverse)
+                d = r.map[(26 + cPos + r.getOuterPosition() - rPos) % 26];
+            else
+                d = r.revMap[(26 + cPos + r.getOuterPosition() - rPos) % 26];
+
+            return (cPos + d) % 26;
+            }
+
+        // Apply the reflector, the part that comes after the rotors
+        private char reflectorMap(char c)
+            {
+            int cPos = (int)c - 65;
+            cPos = (cPos + reflector.map[cPos]) % 26;
+            return alphabet.ToCharArray()[cPos];
+            }
+
+        // Constructor
+        public EnigmaMachine()
+            {
+            plugBoard = new Dictionary<char, char>();
+
+            // Notch and alphabet are fixed on the rotor
+            // First argument is alphabet, second is the turnover notch
+            Rotor rI = new Rotor(rotorIconf, 'Q', "I");
+            Rotor rII = new Rotor(rotorIIconf, 'E', "II");
+            Rotor rIII = new Rotor(rotorIIIconf, 'V', "III");
+            rotors = new Rotor[] { rI, rII, rIII }; // Default ordering of rotors
+            reflector = new Rotor(reflectorAconf, ' ', "");
+            }
+
+        public void setReflector(char conf)
+            {
+            if (conf != 'A' && conf != 'B' && conf != 'C')
+                {
+                throw new ArgumentException("Invalid argument");
+                }
+
+            string wiring = "";
+            switch (conf)
+                {
+                case 'A':
+                    wiring = reflectorAconf;
+                    break;
+                case 'B':
+                    wiring = reflectorBconf;
+                    break;
+                case 'C':
+                    wiring = reflectorCconf;
+                    break;
+                }
+            reflector.setWiring(wiring);
+            }
+
+        // Enter the ring settings and initial rotor positions
+        public void setSettings(char[] rings, char[] grund)
+            {
+            if (rings.Length != rotors.Length || grund.Length != rotors.Length)
+                {
+                throw new ArgumentException("Invalid argument lengths");
+                }
+
+            for (int i = 0; i < rotors.Length; i++)
+                {
+                rotors[i].ring = Char.ToUpper(rings[i]);
+                rotors[i].setOuterChar(Char.ToUpper(grund[i]));
+                }
+            }
+
+        public void setSettings(char[] rings, char[] grund, string rotorOrder)
+            {
+            Rotor rI = null;
+            Rotor rII = null;
+            Rotor rIII = null;
+
+            // Get the current ordering
+            for (int i = 0; i < rotors.Length; i++)
+                {
+                if (rotors[i].name == "I")
+                    rI = rotors[i];
+                if (rotors[i].name == "II")
+                    rII = rotors[i];
+                if (rotors[i].name == "III")
+                    rIII = rotors[i];
+                }
+
+            string[] order = rotorOrder.Split('-');
+
+            // Set the new ordering
+            for (int i = 0; i < order.Length; i++)
+                {
+                if (order[i] == "I")
+                    rotors[i] = rI;
+                if (order[i] == "II")
+                    rotors[i] = rII;
+                if (order[i] == "III")
+                    rotors[i] = rIII;
+                }
+
+            setSettings(rings, grund);
+            }
+
+        public void setSettings(char[] rings, char[] grund, string rotorOrder, char reflectorConf)
+            {
+            setReflector(reflectorConf);
+            setSettings(rings, grund, rotorOrder);
+            }
+
+        // Encrypts or decrypts a message
+        public string runEnigma(string msg)
+            {
+            StringBuilder encryptedMessage = new StringBuilder();
+
+            msg = msg.ToUpper();
+
+            foreach (char c in msg)
+                {
+                encryptedMessage.Append(encryptChar(c));
+                }
+
+            return encryptedMessage.ToString();
+            }
+
+        // Encrypts (or decrypts) a single character
+        private char encryptChar(char c)
+            {
+
+            // Rotate the rotors before scrambling
+            rotateRotors(rotors);
+
+            // Into plugboard from keyboard <--
+            if (plugBoard.ContainsKey(c))
+                {
+                c = plugBoard[c];
+                }
+
+            // Scramble with rotors
+            // First we go all the way through the rotors <--
+            c = rotorMap(c, false);
+
+            // Reflect at the end so we don't just unscramble it again when we go back
+            // If the line below is commented out, the cipher will be equal to the message
+            c = reflectorMap(c);
+
+            // Go back through all the rotors the other way -->
+            c = rotorMap(c, true);
+
+            // Plugboard again, from other direction -->
+            if (plugBoard.ContainsKey(c))
+                {
+                c = plugBoard[c];
+                }
+
+            // Character is now encrypted
+            return c;
+            }
+
+        // Add a character pair into the plugboard
+        public void addPlug(char c, char cc)
+            {
+            if (Char.IsLetter(c) && Char.IsLetter(cc))
+                {
+                c = Char.ToUpper(c);
+                cc = Char.ToUpper(cc);
+                if (c != cc && !plugBoard.ContainsKey(c))
+                    {
+                    plugBoard.Add(c, cc);
+                    plugBoard.Add(cc, c);
+                    }
+                }
+            else
+                {
+                throw new ArgumentException("Invalid character");
+                }
+            }
+        }
+    }
---
+++ EnigmaMachineSLN/EnigmaMachinePrj/Program.cs
@@ -0,0 +1,329 @@
+﻿using System.Diagnostics;
+using System.Security.Cryptography;
+using System.Text.RegularExpressions;
+
+namespace EnigmaMachinePrj
+    {
+    internal class Program
+        {
+        static void Main(string[] args)
+            {
+            EnigmaMachine machine = new EnigmaMachine();
+            EnigmaSettings eSettings = new EnigmaSettings();
+
+            querySettings(eSettings);
+
+            string message = "";
+            Console.Write("Enter message to encrypt: ");
+            message = Console.ReadLine();
+            while (!Regex.IsMatch(message, @"^[a-zA-Z ]+$"))
+                {
+                Console.Write("Only letters A-Z is allowed, try again: ");
+                message = Console.ReadLine();
+                }
+            message = message.Replace(" ", "").ToUpper();
+
+            // Enter settings on machine
+            machine.setSettings(eSettings.rings, eSettings.grund, eSettings.order, eSettings.reflector);
+
+            // The plugboard settings
+            foreach (string plug in eSettings.plugs)
+                {
+                char[] p = plug.ToCharArray();
+                machine.addPlug(p[0], p[1]);
+                }
+
+            // Message encrypt
+            Console.WriteLine();
+            Console.WriteLine("Plain text:\t" + message);
+            string enc = machine.runEnigma(message);
+            Console.WriteLine("Encrypted:\t" + enc);
+
+            // Reset the settings before decrypting!
+            machine.setSettings(eSettings.rings, eSettings.grund, eSettings.order, eSettings.reflector);
+
+            // Message decrypt
+            string dec = machine.runEnigma(enc);
+            Console.WriteLine("Decrypted:\t" + dec);
+            Console.WriteLine();
+
+
+            //Set up all possible options. 
+            var plugs = GenerateCombinations();
+            char[] rings = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray();
+            char[] grund = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray();
+            string[] elements = { "I", "II", "III" };
+            List<string> rotorOrders = new List<string>();
+            foreach (string element1 in elements)
+                {
+                foreach (string element2 in elements)
+                    {
+                    foreach (string element3 in elements)
+                        {
+                        if (element1 != element2 && element2 != element3 && element1 != element3)
+                            {
+                            rotorOrders.Add(element1 + "-" + element2 + "-" + element3);
+                            }
+                        }
+                    }
+                }
+            char[] reflectors = "ABC".ToCharArray();
+
+
+            SingleThread(rings, grund, rotorOrders, reflectors, enc, machine, dec);
+            try
+                {
+                MultyThread(rings, grund, rotorOrders, reflectors, enc, dec);
+                }
+            catch (Exception ex)
+                {
+                Console.WriteLine("--ALL DONE--");
+                }
+
+
+
+
+            Console.WriteLine("--ALL DONE--");
+            Console.ReadLine();
+            }
+
+        private static void querySettings(EnigmaSettings e)
+            {
+            string r;
+            Console.WriteLine("Enigma Machine Emulator\n");
+            Console.Write("Do you want to: [1] Specify settings [2] Use default settings? (Default: [2]): ");
+            r = Console.ReadLine();
+            while (r != "1" && r != "2" && r != "")
+                {
+                Console.Write("Invalid input, enter 1, 2 or 3 ");
+                r = Console.ReadLine();
+                }
+            if (r == "1")
+                {
+                Console.Write("Enter the ring settings (Ex. AAA, MCK, Default: AAA): ");
+                r = Console.ReadLine();
+                if (r == "")
+                    e.rings = new char[] { 'A', 'A', 'A' };
+                else
+                    e.rings = r.ToCharArray();
+
+                Console.Write("Enter the inital rotor start settings (Ex. AAA, MCK, Default: AAA): ");
+                r = Console.ReadLine();
+                if (r == "")
+                    e.grund = new char[] { 'A', 'A', 'A' };
+                else
+                    e.grund = r.ToCharArray();
+
+                Console.Write("Enter the order of the rotors (Ex. I-II-III, III-I-II, Default: I-II-III): ");
+                r = Console.ReadLine();
+                if (r == "")
+                    e.order = "I-II-III";
+                else
+                    e.order = r;
+
+                Console.Write("Enter the reflector to use (A, B, or C, Default: B): ");
+                r = Console.ReadLine();
+                if (r == "")
+                    e.reflector = 'B';
+                else
+                    e.reflector = r.ToCharArray()[0];
+
+                Console.Write("Enter the plugboard configuration (Ex. KH AB CE IJ, Default: None): ");
+                r = Console.ReadLine();
+                if (r == "")
+                    e.plugs.Clear();
+                else
+                    {
+                    string[] plugs = r.Split(' ');
+                    foreach (string s in plugs)
+                        {
+                        e.plugs.Add(s);
+                        }
+                    }
+
+                }
+            else if (r == "2" || r == "")
+                {
+                e.setDefault();
+                }
+
+            Console.WriteLine();
+            }
+
+
+        private static void SingleThread(char[] rings, char[] grund, List<string> rotorOrders, char[] reflectors, string encryptedText, EnigmaMachine machine, string dec)
+            {
+
+
+            System.Diagnostics.Stopwatch stopwatch = new System.Diagnostics.Stopwatch();
+            stopwatch.Start();
+
+            foreach (var reflector in reflectors)
+                {
+                foreach (var rotorOrder in rotorOrders)
+                    {
+                    char[] ringSettings = new char[3];
+                    foreach (var ring0 in rings)
+                        {
+                        foreach (var ring1 in rings)
+                            {
+                            foreach (var ring2 in rings)
+                                {
+                                ringSettings[0] = ring0;
+                                ringSettings[1] = ring1;
+                                ringSettings[2] = ring2;
+
+
+                                char[] grundSettings = new char[3];
+                                foreach (var grunt0 in grund)
+                                    {
+                                    foreach (var grunt1 in grund)
+                                        {
+                                        foreach (var grunt2 in grund)
+                                            {
+
+                                            grundSettings[0] = grunt0;
+                                            grundSettings[1] = grunt1;
+                                            grundSettings[2] = grunt2;
+
+                                            machine.setSettings(ringSettings, grundSettings, rotorOrder, reflector);
+                                            string attempt = machine.runEnigma(encryptedText);
+
+                                            if (attempt == dec)
+                                                {
+                                                stopwatch.Stop();
+                                                Console.WriteLine("----");
+                                                Console.WriteLine("----");
+                                                Console.WriteLine("Found!");
+                                                Console.WriteLine(attempt);
+                                                Console.WriteLine(stopwatch.ElapsedTicks.ToString());
+                                                Console.ReadLine();
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+
+
+
+            }
+
+        private static void MultyThread(char[] rings, char[] grund, List<string> rotorOrders, char[] reflectors, string encryptedText, string dec)
+            {
+            CancellationTokenSource cts = new CancellationTokenSource();
+            CancellationToken token = cts.Token;
+
+            System.Diagnostics.Stopwatch stopwatch = new System.Diagnostics.Stopwatch();
+            stopwatch.Start();
+
+            Parallel.ForEach(reflectors, new ParallelOptions { CancellationToken = token }, (reflector) =>
+            {
+                foreach (var rotorOrder in rotorOrders)
+                    {
+                    char[] ringSettings = new char[3];
+                    Parallel.ForEach(rings, new ParallelOptions { CancellationToken = token }, (ring0) =>
+                    {
+                        foreach (var ring1 in rings)
+                            {
+                            foreach (var ring2 in rings)
+                                {
+                                ringSettings[0] = ring0;
+                                ringSettings[1] = ring1;
+                                ringSettings[2] = ring2;
+
+                                char[] grundSettings = new char[3];
+                                Parallel.ForEach(grund, new ParallelOptions { CancellationToken = token }, (grunt0) =>
+                                {
+                                    foreach (var grunt1 in grund)
+                                        {
+                                        foreach (var grunt2 in grund)
+                                            {
+                                            grundSettings[0] = grunt0;
+                                            grundSettings[1] = grunt1;
+                                            grundSettings[2] = grunt2;
+
+                                            EnigmaMachine machine = new EnigmaMachine();
+                                            machine.setSettings(ringSettings, grundSettings, rotorOrder, reflector);
+                                            string attempt = machine.runEnigma(encryptedText);
+
+                                            if (attempt == dec)
+                                                {
+                                                stopwatch.Stop();
+                                                Console.WriteLine("----");
+                                                Console.WriteLine("Settings:");
+                                                Console.WriteLine(ringSettings);
+                                                Console.WriteLine(grundSettings);
+                                                Console.WriteLine(rotorOrder);
+                                                Console.WriteLine(reflector);
+                                                Console.WriteLine("----");
+                                                Console.WriteLine("Found!");
+                                                Console.WriteLine(attempt);
+                                                Console.WriteLine((stopwatch.ElapsedTicks / Stopwatch.Frequency).ToString());
+                                                cts.Cancel(); // Request cancellation
+                                                return; // Exit current iteration
+                                                }
+
+                                            // Check for cancellation
+                                            token.ThrowIfCancellationRequested();
+                                            }
+                                        }
+                                });
+                                }
+                            }
+                    });
+                    }
+            });
+
+
+
+            }
+
+
+        public static List<string> GenerateCombinations()
+            {
+            char[] alphabet = "abcdefghijklmnopqrstuvwxyz".ToUpperInvariant().ToCharArray();
+            List<string> combinations = new List<string>();
+
+            foreach (char letter1 in alphabet)
+                {
+                foreach (char letter2 in alphabet)
+                    {
+                    combinations.Add(letter1.ToString() + letter2.ToString());
+                    }
+                }
+
+            return combinations;
+            }
+
+        // Short class to hold the settings
+        private class EnigmaSettings
+            {
+            public char[] rings { get; set; }
+            public char[] grund { get; set; }
+            public string order { get; set; }
+            public char reflector { get; set; }
+            public List<string> plugs = new List<string>();
+
+            public EnigmaSettings()
+                {
+                setDefault();
+                }
+
+            public void setDefault()
+                {
+                rings = new char[] { 'A', 'A', 'A' };
+                grund = new char[] { 'A', 'A', 'A' };
+                order = "I-II-III";
+                reflector = 'B';
+                plugs.Clear();
+                }
+            }
+
+
+        }
+
+    }
